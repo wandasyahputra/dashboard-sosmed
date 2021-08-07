7c6985776653147973b4cbd70836231a34b038ba
Wanda Syahputra <gegerku.kucingku@gmail.com>
2021-08-07 13:57:49 +0700

    chore: install redux and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary">Test Button</Button>
       </header>
     </div>
   );
